a7a1478a47d36379275d1081b29026edb322fc86
Dilyara <dilnov2008@mail.ru>
2023-07-26 23:52:02 +0300

    Задание 6-3

--- catalog/management/commands/fill_db.py
+++ catalog/management/commands/fill_db.py
@@ -1,6 +1,8 @@
 from django.core.management import BaseCommand
 from catalog.models import Product, Category
 
+Product.objects.all().delete()
+Category.objects.all().delete()
 
 class Command(BaseCommand):
 
@@ -13,17 +15,17 @@ class Command(BaseCommand):
         category_fill =[]
         for category_item in category_list:
             category_fill.append(Category(**category_item))
-            
+
         Category.objects.bulk_create(category_fill)
         
         product_list = [
-                {'id': '1','name': 'Товар1', 'description': 'Описание1', 'preview': '', 'category':'1','price':'100','created':'20230101','last_upd':''},
-                {'id': '2','name': 'Товар2', 'description': 'Описание2', 'preview': '', 'category':'2','price':'200','created':'20230202','last_upd':''},
-                {'id': '3','name': 'Товар3', 'description': 'Описание3', 'preview': '', 'category':'3','price':'300','created':'20230303','last_upd':''},
+                {'id': '1','name': 'Товар1', 'description': 'Описание1', 'preview': '', 'category': Category(id='1'),'price':'100','created':'20230101','last_upd':''},
+                {'id': '2','name': 'Товар2', 'description': 'Описание2', 'preview': '', 'category':Category(id='2'),'price':'200','created':'20230202','last_upd':''},
+                {'id': '3','name': 'Товар3', 'description': 'Описание3', 'preview': '', 'category':Category(id='3'),'price':'300','created':'20230303','last_upd':''},
                  ]
 
         product_fill =[]
         for product_item in product_list:
             product_fill.append(Product(**product_item))
-        
+
         Product.objects.bulk_create(product_fill)
