df50d38254760fb4775f304cd81f8da18a03de35
Dilyara <dilnov2008@mail.ru>
2023-10-31 13:23:21 +0300

    Задание 4-7

--- blog/templatetags/tags_filters.py
+++ blog/templatetags/tags_filters.py
@@ -3,6 +3,5 @@ from django import template
 register = template.Library()
 
 @register.simple_tag
-@register.filter()
-def mediapath(data) -> str:
-    return data.url if data else '#'
+def mediapath(val):
+    return f'/media/{val}'
