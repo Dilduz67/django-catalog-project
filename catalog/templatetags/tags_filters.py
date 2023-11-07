4f855d0088d1839c0ef3b2869a1ee880edecbc4c
Dilyara <dilnov2008@mail.ru>
2023-11-07 16:31:40 +0300

    Задание 4-8

--- catalog/templatetags/tags_filters.py
+++ catalog/templatetags/tags_filters.py
@@ -6,3 +6,7 @@ register = template.Library()
 @register.filter()
 def mediapath(data) -> str:
     return data.url if data else '#'
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
